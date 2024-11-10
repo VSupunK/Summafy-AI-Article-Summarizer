a2d6047ef5e940f279eb6b5b179a08fb7444206d
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-11-10 13:51:06 +0530

    Update article API service to include RapidAPI key and configure base query

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
-import store from "./services/store.js";
+import { store } from "./services/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- src/services/article.js
+++ src/services/article.js
@@ -1,7 +1,20 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLES_KEY;
+
 export const articleApi = createApi({
   reducerPath: "articleApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+    prepareHeaders: (headers) => {
+      headers.set("x-rapidapi-key", rapidApiKey);
+      headers.set(
+        "x-rapidapi-host",
+        "article-extractor-and-summarizer.p.rapidapi.com"
+      );
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({
     getArticle: builder.query({
       query: (url) => `test`,
