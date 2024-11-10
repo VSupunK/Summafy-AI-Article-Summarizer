c3d9d8a06daea8733aa69f317003a59fd1915e83
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-11-10 11:02:31 +0530

    Change file structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,18 @@ import Demo from "./components/Demo";
 import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+
+      <div className="app">
+        <Hero />
+        <Demo />
+      </div>
+    </main>
+  );
 };
 
 export default App;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { SummafyLogoIco } from "../assets";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex flex-col justify-between items-center">
+        <img src={logo} alt="summafyLogo" />
+      </nav>
+    </header>
+  );
 };
 
 export default Hero;
